8f20b159de3d40f2cc29161948b70b11521bd6ab
iyaiya <hadjouriyad@gmail.com>
2024-01-29 11:52:52 +0100

    nouveau

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //état et variables du jeu
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-display');
+    const ground = document.querySelector('.ground-moving');
+
+let birdleft = 220;
+let birdBottom = 100
+let gravity = 3
+let isGameOver = false
+let score = 0
+
+
+
+
+
+    //fonction du jeu 
+});
